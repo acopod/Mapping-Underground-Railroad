40fec2e472e8e444007a76d02698241c0b01032d
acopod <chenaska919@yahoo.com.tw>
2022-10-28 10:30:59 -0400

    Update config.js

--- MrSimmsEscapeRoute/config.js
+++ MrSimmsEscapeRoute/config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/acopod/cl9r4xmnj001n15p8heixjraz',
+    style: 'mapbox://styles/acopod/cl9rz3rjj000s14qja3ea4y1x',
     accessToken: 'pk.eyJ1IjoiYWNvcG9kIiwiYSI6ImNsOXI4NHEzZTBkbWU0MHFtZGJuaGZkYWMifQ.sZmqyas3S7qPVHJO032xuA',
     showMarkers: true,
     markerColor: '#3FB1CE',
@@ -22,9 +22,9 @@ var config = {
             image: 'images/Water1-superJumbo.jpg',
             description: 'In the fall of 2012, New York City was hit with hurricane that left tens of thousands of public housing residents <a href = "https://www1.nyc.gov/site/nycha/about/press/pr-2012/nycha-has-restored-power-to-buildings-affected-by-hurricane-sandy.page">without power for days or even weeks</a>. New York City Housing Authority public housing developments were affected. <a href="https://furmancenter.org/files/publications/SandysEffectsOnHousingInNYC.pdf">24,000 government-subsidized apartments and 40,000 rent-stabilized apartments were also affected. With rising sea levels from climate change and the <a href="https://www.nytimes.com/2017/04/18/magazine/when-rising-seas-transform-risk-into-certainty.html">promise</a> of more flooding, which areas and <i>who</i> will likely be most affected? How is affordable housing construction responding to this? </p>',
             location: {
-                center: [-73.97269, 40.75511],
-zoom: 11.48,
-pitch: 46.04,
+                center: [-76.84464, 40.81432],
+zoom: 6.35,
+pitch: 0.00,
 bearing: 0.00
             },
             mapAnimation: 'flyTo',
@@ -53,10 +53,12 @@ bearing: 0.00
             image: 'images/2362.webp',
             description: 'The city classifies Red Hook Houses as being in flood zone 1, indicating high flood risk. During Hurricane Sandy in 2012, the development was inundated with 6 feet (1.8 m) of flood water through sewer overflow and high tides.[16][2][17] The storm left residents without power and fresh water for months. This caused leaks, resulting in mold that affected many residents health. Prior to Sandy, various city reports outlined the need for increased flood protection projecting floods as high as 5 feet during storm surges yet NYCHA officials did not anticipate storm surges being that strong.',
             location: {
-                center: [-74.01240, 40.67357],
-                zoom: 14.99,
-                pitch: 44.53,
-                bearing: 84.22,
+center: [-77.53627, 39.27532],
+zoom: 12.58,
+pitch: 60,
+bearing: 0.00,
+speed: 0.5,
+curve: 1,
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -76,7 +78,7 @@ bearing: 0.00
                  ],
             onChapterExit: []
         },
-        /*{
+        {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
@@ -84,10 +86,10 @@ bearing: 0.00
             image: 'images/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [6.15116, 46.20595],
-                zoom: 12.52,
-                pitch: 8.01,
-                bearing: 0.00
+center: [-77.66071, 39.94729],
+zoom: 10.68,
+pitch: 0.00,
+bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -103,16 +105,16 @@ bearing: 0.00
             image: 'images/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-58.54195, -34.71600],
-                zoom: 4,
-                pitch: 0,
-                bearing: 0
+                center: [-77.18563, 40.20027],
+zoom: 10.70,
+pitch: 0.00,
+bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        }*/
+        }
     ]
 };
